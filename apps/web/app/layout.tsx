ed924367a630a43e7919543af699e787c4bd24e3
Saad Haider <mohd.saadhaider@gmail.com>
2026-09-02 11:45:51 +0530

    feat: add Vercel Analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
         </main>
         <Footer />
         <Toaster position="bottom-right" theme="system" />
+        <Analytics />
       </body>
     </html>
   );
